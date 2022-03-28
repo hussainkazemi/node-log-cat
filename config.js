1d765a57d247ca125aec9d0fd651e2bb8ef77587
hussainkazemi <kazemih7287@gmail.com>
2022-03-28 19:26:27 +0430

    add config example

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
 const LogHeloper = require('./log-cat/logHelper')
-const {Colors, Type} = require('./log-cat/logEnum');
+const {Colors} = require('./log-cat/logEnum');
 const config = {
     logPath: "./LogCat.log",
-    printDate: false,
-    showWarning: false,
+    printDate: true, 
+    showWarning: true,
     showError: true,
-    showDebug: false,
+    showDebug: true,
     showInfo: true,
+    showSuccess: true,
     warningColor: Colors.yellow,
     infoColor: Colors.blue,
     debugColor: Colors.cyan,
-    errorColor: Colors.red
+    errorColor: Colors.red,
+    successColor: Colors.green
 }
 const Log = new LogHeloper(config);
 
